ed52096206b4e6dfd21684f59bffc2357fd9352f
ArjanRoos96 <arjanroos@outlook.com>
2024-02-29 21:54:32 +0100

    add refresh page back

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      {/* <meta http-equiv="refresh" content="10" ></meta> */}
+      <meta http-equiv="refresh" content="10" ></meta>
       <div className="w-full flex">
         <div className="w-5/12">
           <PickUp data={readyOrders ? readyOrders : []} />
